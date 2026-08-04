de04ce437526d205aa4eae0100d4da8431ccd3fe
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2026-08-04 18:11:37 -0400

    Update about.jq.xml for changes to jq-html-home format

--- jq-html-home/shortcuts/about.js
+++ jq-html-home/shortcuts/about.js
@@ -1,5 +1,5 @@
 const ab = document.querySelector('template').content.children[0];
 document.getElementById('a').textContent = ab.childNodes[0].textContent;
 document.getElementById('t').textContent = ab.querySelector('txt').textContent;
-document.getElementById('u').setAttribute('href', ab.querySelector('uri').textContent);
+document.getElementById('u').href = ab.querySelector('uri').textContent;
 document.getElementById('j').textContent = JSON.parse(ab.querySelector('json').textContent);
